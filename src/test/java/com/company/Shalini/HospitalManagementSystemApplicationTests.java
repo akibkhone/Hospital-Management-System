e1655a41611ac4f16f07c7266606d09ce57fb709
Akib Khan <90907996+akibkhone@users.noreply.github.com>
2022-06-08 23:38:21 +0530

    Add files via upload

---
+++ src/test/java/com/company/Shalini/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Shalini;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
